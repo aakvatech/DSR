b5dbec22408a028951178082d9575d52a6ab5893
Info Aakvatech <info@aakvatech.com>
2019-09-08 11:03:50 +0000

    feat: All doctypes created based on discussion

--- dsr/config/docs.py
+++ dsr/config/docs.py
@@ -8,4 +8,4 @@ Configuration for docs
 # sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
-	context.brand_html = "Dsr"
+	context.brand_html = "Daily Sales Report"
---
+++ dsr/config/dsr.py
@@ -0,0 +1,120 @@
+from __future__ import unicode_literals
+from frappe import _
+import frappe
+
+
+def get_data():
+	config = [
+		{
+			"label": _("Daily Sales Documents"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Attendant Pump",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Pump Meter Reading",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Dispensed for Office Use",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Expense Record",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Credit Sales",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Dip Reading",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Cash Deposited",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Credit Sales Collection",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Fuel Stock Receipts",
+					"onboard": 0,
+				},
+				{
+					"type": "doctype",
+					"name": "Inspection Report",
+					"onboard": 0,
+				},
+			]
+		},
+		{
+			"label": _("Daily Sales Masters"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Pump",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Attendant",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Station Shift",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Fuel Item",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Credit Customer",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Expense Type",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Fuel Tank",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Station Details",
+					"onboard": 1,
+				},
+			]
+		},
+		{
+			"label": _("Reports"),
+			"items": [
+				{
+					"type": "query-report",
+					"name": "Register",
+					"doctype": "Bill",
+					"onboard": 0,
+				},
+			]
+		},
+	]
+	return config
