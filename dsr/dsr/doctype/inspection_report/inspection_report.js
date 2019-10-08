fc4a0dbfeb84e5f425a9d2c0f17a1f530ef07441
Info Aakvatech <info@aakvatech.com>
2019-10-08 08:58:39 +0000

    feat: First five issues done

--- dsr/dsr/doctype/inspection_report/inspection_report.js
+++ dsr/dsr/doctype/inspection_report/inspection_report.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Inspection Report', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function(frm) {
+		console.log("yes")
+		frappe.call({
+			method: "frappe.client.get_list",
+			args: {
+				doctype: "Inspection Report",
+				fields: ["owner"],
+				order_by: "inspection_date"
+				},
+			callback: function (r) {
+				console.log("Yes")
+			}
+		});
+	}
 });
