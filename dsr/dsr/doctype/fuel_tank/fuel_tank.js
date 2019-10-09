b2f7b7d470372520be21ba9aeee5786cba59692f
Info Aakvatech <info@aakvatech.com>
2019-10-09 04:13:49 +0000

    feat: Added station name in automname of fuel tank nad moved custom scripts to .js files

--- dsr/dsr/doctype/fuel_tank/fuel_tank.js
+++ dsr/dsr/doctype/fuel_tank/fuel_tank.js
@@ -2,7 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Fuel Tank', {
-	// refresh: function(frm) {
+	refresh(frm) {
+		// your code here
+	}
+})
 
-	// }
-});
+frappe.ui.form.on('Fuel Tank Calibration', {
+	litres:function(frm,cdt,cdn) {
+	    var child = locals[cdt][cdn];
+	    console.log(cur_frm);
+	    if(cur_frm.doc.tank_capacity){
+            var capacity = cur_frm.doc.tank_capacity * 0.95 ;
+            if (child.litres){
+                    if(parseFloat(child.litres) > capacity){
+                        frappe.model.set_value(cdt,cdn,"litres",'')
+                        frappe.throw(__("Should not exceed fuel tank capacity's 95%"))
+                    }
+            
+            }
+	    }
+	    else{
+	        frappe.throw(__("Set Tank Capasity First"))
+	    }
+	}
+})
--- dsr/dsr/doctype/inspection_report/inspection_report.js
+++ dsr/dsr/doctype/inspection_report/inspection_report.js
@@ -2,18 +2,81 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Inspection Report', {
-	onload: function(frm) {
-		console.log("yes")
-		frappe.call({
-			method: "frappe.client.get_list",
-			args: {
-				doctype: "Inspection Report",
-				fields: ["owner"],
-				order_by: "inspection_date"
-				},
-			callback: function (r) {
-				console.log("Yes")
-			}
-		});
+	onload: function(frm,cdt,cdn) {
+        if(frm.doc.__islocal){
+            frappe.model.set_value(cdt,cdn,"inspected_by",frappe.session.user)
+    		frappe.call({
+    			method: "frappe.client.get_list",
+    			args: {
+    				doctype: "Inspection Report",
+    				fields: ["owner","inspection_date"],
+    				order_by: "inspection_date desc",
+    				limit_page_length: 1
+    				},
+    			callback: function (r) {
+    			    if(r.message){
+    				    console.log(r.message[0].owner)
+    				    frappe.model.set_value(cdt,cdn,"last_inspection_by",r.message[0].owner)
+    				    frappe.model.set_value(cdt,cdn,"last_inspection_date",r.message[0].inspection_date)
+    				}
+    			}
+    		});
+    		frappe.call({
+    			method: "frappe.client.get_list",
+    			args: {
+    				doctype: "Shift",
+    				fields: ["name","fuel_station"],
+    				order_by: "creation desc",
+    				limit_page_length: 1
+    				},
+    			callback: function (r) {
+    			    if(r.message){
+    				    frappe.model.set_value(cdt,cdn,"shift",r.message[0].name)
+    				    frappe.model.set_value(cdt,cdn,"fuel_station",r.message[0].fuel_station)
+
+    				}
+    			}
+    		});    		
+
+        }
+	},
+	fuel_station:function(frm,cdt,cdn){
+	    var doc = locals[cdt][cdn]
+	    if(doc.fuel_station){
+	        frappe.call({
+	            method:"frappe.client.get_list",
+	            args:{
+	                doctype:'Fuel Tank',
+	                filters:{'fuel_station':doc.fuel_station},
+	                fields:["name","tank_capacity"]
+	            },
+	            callback:function(r){
+	                frm.clear_table("inspection_report_tank");
+	                r.message.forEach(d => {
+                        var child = frm.add_child("inspection_report_tank")
+                        frappe.model.set_value(child.doctype,child.name,"fuel_tank",d.name);
+                        frappe.model.set_value(child.doctype,child.name,"tank_capacity",d.tank_capacity);
+	                })
+	                refresh_field("inspection_report_tank");
+	            }
+	        })
+	        frappe.call({
+	            method:"frappe.client.get_list",
+	            args:{
+	                doctype:'Pump',
+	                filters:{'fuel_station':doc.fuel_station},
+	                fields:["name"]
+	            },
+	            callback:function(r){
+	                frm.clear_table("inspection_report_dispenser");
+	                r.message.forEach(d => {
+	                    var child = frm.add_child("inspection_report_dispenser");
+	                   frappe.model.set_value(child.doctype,child.name,"pump",d.name);
+	                })
+	                refresh_field("inspection_report_dispenser");
+
+	            }
+	        })
+	    }
 	}
 });
