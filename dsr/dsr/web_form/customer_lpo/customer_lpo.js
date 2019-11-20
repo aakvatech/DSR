f5bc3d45bd239ff7f733b089ce41850a72930778
Info Aakvatech <info@aakvatech.com>
2019-11-20 17:03:52 +0000

    feat: Yet another round of additions including fuel sation access rights

--- dsr/dsr/web_form/customer_lpo/customer_lpo.js
+++ dsr/dsr/web_form/customer_lpo/customer_lpo.js
@@ -2,27 +2,3 @@ frappe.ready(function() {
 	// bind events here
 
 });
-
-frappe.web_form.on('fuel_station', (field, value) => {
-	filterFuelItem(fuel_station);
-});
-
-function filterFuelItem(fuel_station) {
-	$.ajax({
-		type: 'GET', 
-		url: 'api/resource/Fuel Item?filters=[["Fuel Item","fuel_station","=","${fuel_station}"]]',
-		success: function(result) {
-			var options = [];
-			for (var i = 0; i < result.data.length; i++) {
-				options.push({
-					'label': result.data[i].name,
-					'value': result.data[i].name
-				});
-			}
-			var field = frappe.web_form.field_group.get_field('fuel_item');
-			field._data = options;
-			field.refresh();
-		}
-	});
-	frappe.msgprint(options)
-};	
