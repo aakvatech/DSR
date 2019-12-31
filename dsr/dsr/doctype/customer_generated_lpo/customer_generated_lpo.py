51e3db28acc41868a169f97d3d761cdf12b7a15a
Info Aakvatech <info@aakvatech.com>
2019-12-31 16:02:23 +0000

    fix: Removed code creating error on submitting Customer LPO

--- dsr/dsr/doctype/customer_generated_lpo/customer_generated_lpo.py
+++ dsr/dsr/doctype/customer_generated_lpo/customer_generated_lpo.py
@@ -7,10 +7,11 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 
 class CustomerGeneratedLPO(Document):
-	def on_submit(self):
-		if self.other_station_cash_record:
-			balance_qty = frappe.db.get_value("Other Station Credit",self.other_station_cash_record,"balance_qty")
-			if balance_qty:
-				if balance_qty < self.quantity:
-					frappe.throw(_("Balance Qty is {0} not sufficient and Applied Qty is {1}").format(balance_qty,self.quantity))
-				frappe.db.set_value("Other Station Credit",self.other_station_cash_record,"balance_qty",balance_qty-self.quantity)
+	# def on_submit(self):
+	# 	if self.other_station_cash_record:
+	# 		balance_qty = frappe.db.get_value("Other Station Credit",self.other_station_cash_record,"balance_qty")
+	# 		if balance_qty:
+	# 			if balance_qty < self.quantity:
+	# 				frappe.throw(_("Balance Qty is {0} not sufficient and Applied Qty is {1}").format(balance_qty,self.quantity))
+	# 			frappe.db.set_value("Other Station Credit",self.other_station_cash_record,"balance_qty",balance_qty-self.quantity)
+	pass
