5a8b3344c0b58c87d54345e3a9045b7d405f03b5
Info Aakvatech <info@aakvatech.com>
2020-02-17 08:22:58 +0000

    feat: Added cluster manager and station manager in Customer Generated LPO

--- dsr/dsr/doctype/customer_generated_lpo/customer_generated_lpo.py
+++ dsr/dsr/doctype/customer_generated_lpo/customer_generated_lpo.py
@@ -3,15 +3,8 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class CustomerGeneratedLPO(Document):
-	# def on_submit(self):
-	# 	if self.other_station_cash_record:
-	# 		balance_qty = frappe.db.get_value("Other Station Credit",self.other_station_cash_record,"balance_qty")
-	# 		if balance_qty:
-	# 			if balance_qty < self.quantity:
-	# 				frappe.throw(_("Balance Qty is {0} not sufficient and Applied Qty is {1}").format(balance_qty,self.quantity))
-	# 			frappe.db.set_value("Other Station Credit",self.other_station_cash_record,"balance_qty",balance_qty-self.quantity)
 	pass
